a2abd2948e6dedca760a12780ae17e713b4cab4d
adil-jr <adil.jr@outlook.com>
2025-11-02 20:19:51 -0300

    feat: add `DefaultButton` component with styles and integrate it into the form in `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
 import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
 import { DefaultInput } from "./components/DefaultInput";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
+import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
 
 import "./styles/theme.css";
 import "./styles/global.css";
@@ -43,7 +45,8 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} color="green" />
+            <DefaultButton icon={<StopCircleIcon />} color="red" />
           </div>
         </form>
       </Container>
