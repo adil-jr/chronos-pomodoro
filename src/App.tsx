f50f12e36729f43a417461bf6ef31adc827fba63
adil-jr <adil.jr@outlook.com>
2025-11-02 20:29:19 -0300

    feat: add `Footer` component with styles and integrate it into `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { CountDown } from "./components/CountDown";
 import { Cycles } from "./components/Cycles";
 import { DefaultButton } from "./components/DefaultButton";
 import { DefaultInput } from "./components/DefaultInput";
+import { Footer } from "./components/Footer";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
-import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
+import { PlayCircleIcon } from "lucide-react";
 
 import "./styles/theme.css";
 import "./styles/global.css";
@@ -45,10 +46,13 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon />} color="green" />
-            <DefaultButton icon={<StopCircleIcon />} color="red" />
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
