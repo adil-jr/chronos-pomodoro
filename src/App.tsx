351c115fe4ae4d28abd75efe4ddb4803734e8843
adil-jr <adil.jr@outlook.com>
2025-11-02 19:07:42 -0300

    feat: add `DefaultInput` component and integrate it into the form in `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 
@@ -24,8 +25,7 @@ export function App() {
       <Container>
         <form className="form" action="">
           <div className="formRow">
-            <label htmlFor="meuInput">task</label>
-            <input id="meuInput" type="text" />
+            <DefaultInput id="meuInput" type="text" />
           </div>
 
           <div className="formRow">
