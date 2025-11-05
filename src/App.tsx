4b76dfd5119e2e2ca64b36ee0650c8c3efdf8908
adil-jr <adil.jr@outlook.com>
2025-11-05 18:07:38 -0300

    feat(context): implement `TaskContext.tsx` and  `TaskContextProvider.tsx` for state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import { Home } from "./pages/Home";
-import { TaskStateModel } from "./models/TaskStateModel";
-import { useState } from "react";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 
 import "./styles/theme.css";
 import "./styles/global.css";
 
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: "00:00",
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
-
 export function App() {
-  const [state, setState] = useState(initialState);
-
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
