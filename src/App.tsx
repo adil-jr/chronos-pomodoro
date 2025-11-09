4a3bdbccb237faa83a0a2dcb8d4ef05ba86342e2
adil-jr <adil.jr@outlook.com>
2025-11-09 18:41:01 -0300

    feat: add `react-router` for routing and update navigation links

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,22 @@ import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider"
 import "./styles/theme.css";
 import "./styles/global.css";
 import { MessagesContainer } from "./components/MessagesContainer";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { AboutPomodoro } from "./pages/AboutPomodoro";
+import { NotFound } from "./pages/NotFound";
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
+
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </MessagesContainer>
     </TaskContextProvider>
   );
